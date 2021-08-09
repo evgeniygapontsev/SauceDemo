6f48e4fe897116443967410cd0dee97641785ad4
evgeniygapontsev <gapontsev91@gmail.com>
2021-08-09 11:15:23 +0300

    Locators and Test

---
+++ src/test/java/com/saucedemo/locator/SauceDemoTest.java
@@ -0,0 +1,41 @@
+package com.saucedemo.locator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SauceDemoTest {
+
+    @Test
+    public void logInTest() {
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "F:\\QA12WorkSpace\\Webdriver1\\chromedriver.exe"
+        );
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        WebElement username = driver.findElement(By.name("user-name"));
+        username.sendKeys("standard_user");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("secret_sauce");
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit'][value='Login']"));
+        loginButton.click();
+
+        WebElement addToCart = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        addToCart.click();
+        WebElement shoppingCart = driver.findElement(By.className("shopping_cart_link"));
+        shoppingCart.click();
+
+        WebElement confirmationProductNameElement = driver.findElement(By.className("inventory_item_name"));
+        String productName = confirmationProductNameElement.getText();
+        Assert.assertEquals(productName, "Sauce Labs Backpack");
+
+        WebElement confirmationPriceElement = driver.findElement(By.className("inventory_item_price"));
+        String itemPrice = confirmationPriceElement.getText();
+        Assert.assertEquals(itemPrice, "$29.99");
+
+    }
+}
